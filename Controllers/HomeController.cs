4c2480e87ca5db7d58299a0fb3e9b1e7d90de841
omar Aljuboori <omar.m10@hotmail.com>
2023-05-12 09:30:03 +0200

    f

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -40,23 +40,29 @@ public class HomeController : Controller
         return View();
     }
 
-    [HttpGet]
     public IActionResult SavePoint()
     {
+        _logger.LogInformation("---SavePoint---");
         return View();
     }
 
-    [HttpPost]
-    public ActionResult Point(RunningPointInfo runningPointInfo)
+    [HttpPost("info")]
+    public ActionResult Point(int userId, int runningInfoId, double x, double y)
     {
-        var result = _context.Add(runningPointInfo);
+        _logger.LogInformation("---x---");
+        _logger.LogInformation("---y---");
+        _logger.LogInformation("---userId---");
+        _logger.LogInformation("---runningInfos---");
+
+        var result = _context.Add(new RunningPointInfo
+        {
+            X = x,
+            Y = y,
+            User = _context.Users.Find(userId),
+            RunningInfo = _context.RunningInfos.Find(runningInfoId)
+        });
         _context.SaveChanges();
-        return View("Point");
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
+        return View("SavePoint");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
