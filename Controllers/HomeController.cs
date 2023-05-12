91aae27b2e003625ef5e2336052a5671f06da744
omar Aljuboori <omar.m10@hotmail.com>
2023-05-12 09:52:00 +0200

    hjhjh

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -32,7 +32,7 @@ public class HomeController : Controller
     {
         var result = _context.Add(user);
         _context.SaveChanges();
-        return View("Thanks");
+        return View("Thanks", result);
     }
     public IActionResult Register()
     {
